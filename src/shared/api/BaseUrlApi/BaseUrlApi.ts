cc6e8df3a2e3452d4201f723f5649cab6742f23a
Andrey Shatskikh <ponchic-09@mail.ru>
2024-02-17 14:49:22 +0300

    Изменен BASE_URL

--- src/shared/api/BaseUrlApi/BaseUrlApi.ts
+++ src/shared/api/BaseUrlApi/BaseUrlApi.ts
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://127.0.0.1:3000';
-export const BASE_URL = 'http://158.160.110.125';
+// export const BASE_URL = 'http://158.160.110.125';
+export const BASE_URL = 'http://aida-crm.online';
