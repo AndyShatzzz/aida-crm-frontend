11464e359ef6dcb73ccc9e03e7d5c05baccc010a
Andrey Shatskikh <ponchic-09@mail.ru>
2025-02-25 17:00:50 +0300

    feat(localHost + tablePosition): fix bug with first add table + fix local hosting with macbook Andy with base_url

--- src/shared/api/BaseUrlApi/BaseUrlApi.ts
+++ src/shared/api/BaseUrlApi/BaseUrlApi.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'http://127.0.0.1:3000';
+// export const BASE_URL = 'http://127.0.0.1:3000'; // default ip
 // export const BASE_URL = 'http://158.160.110.125';
 // export const BASE_URL = 'https://aida-crm.online';
-export const BASE_URL = 'http://192.168.0.59:3000';
+export const BASE_URL = 'http://192.168.0.59:3000'; // Andy's macbook ip
